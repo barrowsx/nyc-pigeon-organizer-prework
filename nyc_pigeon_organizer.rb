95458687811df4f3c8875d8d60d4aee4990c9fb3
barrowsx <wwboyer14@mail.strakejesuit.org>
2017-06-06 16:44:06 -0400

    highly optimized :O

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,29 +1,15 @@
-require 'pry'
-
 def nyc_pigeon_organizer(data)
-  # write your code here!
-  pigeon_name_array = []
-  pigeon_list = {}
-  data.each do |attributes, variants|
-    variants.each do |type, pigeons|
-      pigeon_name_array << pigeons
-    end
+  names = data.map {|key, value| value.values}.flatten.uniq
+  new_hash = names.each_with_object({}) do |name, h|
+    h[name] = Hash[data.keys.map{|x| [x, []]}]
   end
-  pigeon_name_array.flatten!.uniq!
-  pigeon_name_array.each do |pigeon|
-    pigeon_list[pigeon] = {}
-  end
-  pigeon_list.each do |pigeon, attributes|
-    data.each do |att, var|
-      pigeon_list[pigeon][att] = []
-      var.each do |type, pigeons|
-        pigeons.each do |name|
-          if pigeon == name
-            pigeon_list[pigeon][att] << type.to_s
-          end
+  data.keys.each_with_object(new_hash) do |type, struct|
+    names.each do |pigeon|
+      data[type].keys.each do |trait|
+        if data[type][trait].include?(pigeon)
+          struct[pigeon][type] << trait.to_s
         end
       end
     end
   end
-  pigeon_list
 end
